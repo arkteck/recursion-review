8192e76cc32f654a764f5e5c8a495d260e4fd6df
arkteck <arkteck@gmail.com>
2022-04-11 20:08:56 -0400

    Bare minimum requirements completed.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var body = document.body;
+  var res = [];
+
+  var helper = function(element) {
+    if (element.classList && element.classList.contains(className)) {
+      res.push(element);
+    }
+
+    for (var childNode of element.childNodes) {
+      if (!childNode) {
+      }
+      helper(childNode);
+    }
+
+  };
+
+  helper(body);
+  return res;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var res = '';
+
+  var helper = function(node) {
+    if (node === undefined || typeof node === 'function' ) {
+      return '';
+    }
+    if (Array.isArray(node)) {
+      res += '[';
+      for (var i = 0; i < node.length; i++) {
+        helper(node[i]);
+        if (i < node.length - 1) {
+          res += ',';
+        }
+      }
+      res += ']';
+    } else if (typeof node === 'object') {
+      if (node === null) {
+        res += 'null';
+      } else {
+        res += '{';
+        var entries = Object.entries(node);
+        for (var i = 0; i < entries.length; i++) {
+          if (entries[i][1] === undefined || typeof entries[i][1] === 'function') {
+            continue;
+          }
+          res += '"' + entries[i][0] + '":';
+          helper(entries[i][1]);
+          if (i < entries.length - 1) {
+            res += ',';
+          }
+        }
+        res += '}';
+      }
+    } else if (typeof node === 'number') {
+      res += node.toString();
+    } else if (typeof node === 'string') {
+      res += '"' + node + '"';
+    } else if (typeof node === 'boolean') {
+      res += node ? 'true' : 'false';
+    } else {
+      res += '';
+    }
+  };
+
+  helper(obj);
+  return res;
 };
